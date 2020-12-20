93f959618d52721bdf974be185235d576675bc72
EmilioMendoza <Ironislife39@gmail.com>
2020-12-20 00:43:22 -0500

    Stack overflow

---
+++ client.py
@@ -0,0 +1,121 @@
+import sys
+import pygame
+import socket
+pygame.init()
+
+"""
+My idea for this is just a space invaders game sort of thing
+"""
+
+
+HEADER = 64
+PORT = 5050
+FORMAT = "utf-8"
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "192.168.2.100"
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+WIDTH, HEIGHT = (1500, 800)
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space invaders LAN game")
+
+
+class player(object):
+    def __init__(self):
+        self.groundPadding = 30
+        self.x = 0
+        self.y = HEIGHT - 90 - self.groundPadding
+        self.serverx = 0
+        self.servery = 0
+        self.vel = 12
+        self.width = 120
+        self.height = 90
+        self.image = pygame.transform.scale(pygame.image.load("photos/player/player.png"), (self.width, self.height))
+        self.bullets = []
+        self.playernum = 0
+
+    def getPosFromServer(self):
+        playernum = client.recv(1024).decode(FORMAT)
+        self.playernum = playernum
+        message = "getpos".encode(FORMAT)
+        client.send(message)
+        pos = client.recv(4096).decode(FORMAT)
+        print(type(pos))
+        pos = eval(pos)
+        if self.playernum == 1:
+            self.serverx = pos[0]
+            self.servery = pos[1]
+        else:
+            self.serverx = pos[2]
+            self.servery = pos[3]
+
+    def setglobalpos(self):
+        pos = ""
+        client.send("setpos".encode(FORMAT))
+        pos = eval(client.recv(1024).decode(FORMAT))
+        client.send(str((self.x, self.y)).encode(FORMAT))
+        #print(pos)
+
+    def draw(self, win):
+        win.blit(self.image, (self.x, self.y))
+
+    def shoot(self):
+        self.bullets.append(bullet(self.x, self.y, self, len(self.bullets) - 1))
+
+    def controls(self):
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_d]:
+            self.x += self.vel
+        elif pressed[pygame.K_a]:
+            self.x -= self.vel
+
+        if pressed[pygame.K_w]:
+            self.shoot()
+
+
+class bullet(object):
+    def __init__(self, xpPosition, ypPosition, p, indexPos):
+        self.xcurrentPosition = xpPosition + p.width / 2  # Set x to be in the middle of the player at all times
+        self.ycurrentPosition = ypPosition
+        self.vel = 1
+        self.player = p
+        self.indexPos = indexPos
+
+    def shoot(self):
+        if self.ycurrentPosition >= 0:
+            self.ycurrentPosition -= self.vel
+        else:
+            self.player.bullets.pop(0)
+
+    def draw(self):
+        pygame.draw.circle(win, (255, 255, 255), (self.xcurrentPosition, self.ycurrentPosition), 10)
+
+
+def redrawGameWindow(win):
+    win.fill((0, 0, 0))
+    for bullet in p.bullets:
+        bullet.shoot()
+        bullet.draw()
+    p.draw(win)
+    pygame.display.update()
+
+
+p = player()
+
+clock = pygame.time.Clock()
+run = True
+while run:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    #p.getPosFromServer()
+    p.controls()
+    p.getPosFromServer()
+    p.setglobalpos()
+    redrawGameWindow(win)
+
+pygame.quit()
---
+++ server.py
@@ -0,0 +1,91 @@
+import socket
+import threading
+
+HEADER = 64
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())  # Will automatically find local ip address
+ADDR = (SERVER, PORT)
+FORMAT = "utf-8"
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+clients = []
+
+
+class playerclass():
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+
+
+player1 = playerclass()
+player2 = playerclass()
+players = 0
+
+
+def handle_client(conn, addr):
+    global player1, player2, players
+    player = 0
+    thingToReturn = ""
+    connected = True
+    print(f"[CONNECTION] {addr}")
+    while connected:
+        if players == 0:
+            player = 1
+            players = 1
+        elif players == 1:
+            player = 2
+            players = 2
+        conn.send(str(player).encode(FORMAT))
+        print(players)
+        data = conn.recv(1024)
+        if not data:
+            break
+        #print(f"[COMMAND] {addr}, Command: {str(data)}")
+        """if player1 is not None:
+            player = 2
+            player2 = playerclass()
+        else:
+            player = 1
+            player1 = playerclass()"""
+
+        data = data.decode(FORMAT)
+        if data == "getpos":
+            thingToReturn = str([player1.x, player1.y, player2.x, player2.y])
+        if data == "setpos":
+            getclientpos(conn, player)
+        conn.send(thingToReturn.encode())
+    players -= 1
+    conn.close()
+
+
+def getclientpos(conn, playernum):
+    conn.send("pos".encode(FORMAT))
+    pos = conn.recv(1024).decode()
+    pos = eval(pos)
+    if playernum == 1:
+        player1.x = pos[0]
+        player1.y = pos[1]
+    else:
+        player2.x = pos[0]
+        player2.y = pos[1]
+    print(f"Player1: {player1.x}, {player1.y}")
+    try:
+        print(f"Player2: {player2.x}, {player2.y}")
+    except:
+        print("Player2 not initalized")
+
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+
+
+print("[STARTING] server is starting...")
+start()
